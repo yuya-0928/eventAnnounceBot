722efcea05c3674515f175eefa8f7f1649088e8c
yuni-0928 <yuniyuni0928@gmail.com>
2023-06-20 21:07:51 +0900

    formData.tsの型を厳密に書く

--- backend/modules/formData.ts
+++ backend/modules/formData.ts
@@ -1,23 +1,29 @@
-export const formData = (req: any) => {
+import express from "express";
+
+type TypeFormData = {
+  date: string;
+  numberOfSessions: string;
+  dateAry: string[];
+  isTestMode: boolean;
+  textData: string;
+};
+
+export const formData = (req: express.Request): TypeFormData => {
   console.log(req.body);
-  const numberOfSessions = req.body.numberOfSessions ?? '';
-  const date = req.body.date ?? '';
-  const isTestModeToggle = req.body.isTestModeToggle ?? '';
-  const textData = req.body.textData ?? '';
-  // console.log({
-  //   numberOfSessions: numberOfSessions,
-  //   date: date,
-  //   isTestMote: isTestMote,
-  // });
+  const numberOfSessions = req.body.numberOfSessions ?? "";
+  const date = req.body.date ?? "";
+  const isTestModeToggle = req.body.isTestModeToggle ?? "";
+  const textData = req.body.textData ?? "";
   const dateAry = date.split("-");
   const isTestMode = isTestModeToggle === "on" ?? false;
 
-  // console.log({
-  //   date: date,
-  //   numberOfSessions: numberOfSessions,
-  //   dateAry: dateAry,
-  //   isTestMode: isTestMode,
-  // });
+  console.log({
+    date: date,
+    numberOfSessions: numberOfSessions,
+    dateAry: dateAry,
+    isTestMode: isTestMode,
+    textData: textData,
+  });
 
   return {
     date: date,
