00da0d1b23a811e7978aced1589735ccffdd976c
yuni-0928 <yuniyuni0928@gmail.com>
2023-07-10 01:03:40 +0900

    serverでlistenするポートを3001番にしたので、そこにリクエストを送るようにコードを修正する

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
       <h1>エンジニア作業飲み集会通知システム</h1>
       <a href="http://localhost:3001/login">ログインする</a>
       <h2>Tweet&Discord通知&イベントカレンダー登録</h2>
-      <form id="message_form" method="POST" action="/post_announcement">
+      <form id="message_form" method="POST" action="http://localhost:3001/post_announcement">
         <input type="number" name="numberOfSessions" />
         <input type="date" name="date" />
         <input type="checkbox" name="isTestModeToggle" id="test-mode" />
@@ -18,7 +18,7 @@ function App() {
       <form
         id="message_form"
         method="POST"
-        action="/post_announcement_only_discord"
+        action="http://localhost:3001/post_announcement_only_discord"
       >
         <input type="number" name="numberOfSessions" />
         <input type="checkbox" name="isTestModeToggle" id="test-mode" />
