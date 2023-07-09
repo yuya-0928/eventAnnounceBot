d678c40d6098b8f3f0b0a658fb3bef62a1d3e9ad
yuni-0928 <yuniyuni0928@gmail.com>
2023-07-10 00:56:06 +0900

    ログインボタンのURLを変更する

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,7 @@ function App() {
   return (
     <>
       <h1>エンジニア作業飲み集会通知システム</h1>
-      <form method="GET" action="/login">
-        <input type="submit" value="ログインする" />
-      </form>
+      <a href="http://localhost:3001/login">ログインする</a>
       <h2>Tweet&Discord通知&イベントカレンダー登録</h2>
       <form id="message_form" method="POST" action="/post_announcement">
         <input type="number" name="numberOfSessions" />
