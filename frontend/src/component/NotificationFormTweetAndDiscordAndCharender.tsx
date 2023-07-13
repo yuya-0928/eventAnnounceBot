07abaf627569d476f0a7b99807f15d3fb6ff4ee5
yuni-0928 <yuniyuni0928@gmail.com>
2023-07-14 00:15:11 +0900

    開催数の数値を入力するフォームをmaterial uiに置き換える

--- frontend/src/component/NotificationFormTweetAndDiscordAndCharender.tsx
+++ frontend/src/component/NotificationFormTweetAndDiscordAndCharender.tsx
@@ -1,3 +1,4 @@
+import { TextField as MuiTextField } from "@material-ui/core";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { Checkbox } from "@mui/material";
@@ -12,6 +13,11 @@ export const NotificationFormTweetAndDiscordAndCharender = () => {
         method="POST"
         action="http://localhost:3001/post_announcement"
       >
+        <MuiTextField
+          type="number"
+          name="numberOfSessions"
+          variant="outlined"
+        />
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <DatePicker />
         </LocalizationProvider>
