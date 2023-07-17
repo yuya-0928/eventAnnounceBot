aa4670315e54624032f532f4387162be7ba4902c
yuni-0928 <yuniyuni0928@gmail.com>
2023-07-18 00:16:42 +0900

    EventName.tsxの型指定を変更する

--- frontend/src/component/VrcEventCalenderUrlGenerator/Form/EventName.tsx
+++ frontend/src/component/VrcEventCalenderUrlGenerator/Form/EventName.tsx
@@ -3,10 +3,11 @@ import {
   TextField as MuiTextField,
   FormControl as MuiFormControl,
 } from "@material-ui/core";
+import { ChangeEventHandler } from "react";
 
 type Props = {
   value: string;
-  onChange: () => void;
+  onChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>;
 };
 
 export const EventName = ({ value, onChange }: Props) => {
