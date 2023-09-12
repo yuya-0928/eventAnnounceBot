e51e7b8e2ff41f5a93effa77027d3f757c451d15
yuya-0928 <y.hirako0928@gmail.com>
2023-09-12 23:39:12 +0900

    コンフリクトの解消

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <>
       <h1>エンジニア作業飲み集会通知システム</h1>
-      <a href="http://localhost:3001/login">ログインする</a>
       <VrcEventCalenderUrlGenerator />
       <NotificationFormTweetAndDiscordAndCharender />
       <NotificationFormDiscord />
