7e01bb514f6516037fc9f75dc5c4b116a530b6c4
yuya-0928 <y.hirako0928@gmail.com>
2023-09-12 23:45:12 +0900

    不要な機能を削除

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,9 @@
-import { NotificationFormDiscord } from "./component/NotificationFormDiscord";
-import { NotificationFormTweetAndDiscordAndCalender } from "./component/NotificationFormTweetAndDiscordAndCalender";
 import { VrcEventCalenderUrlGenerator } from "./component/VrcEventCalenderUrlGenerator";
 
 function App() {
   return (
     <>
       <h1>エンジニア作業飲み集会通知システム</h1>
-      <a href="http://localhost:3001/login">ログインする</a>
-      <NotificationFormTweetAndDiscordAndCalender />
-      <NotificationFormDiscord />
       <VrcEventCalenderUrlGenerator />
     </>
   );
